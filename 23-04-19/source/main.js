c7fb20ef64b7bca66c05cc1da5d483fc353b644d
Luna <apecxer@gmail.com>
2019-04-24 19:15:16 -0500

    dom

--- 23-04-19/source/main.js
+++ 23-04-19/source/main.js
@@ -5,31 +5,47 @@
   //return MXN/19.37;
 //}
 
-var agencia =[
-  [ "keonnisegg","rojo","1200","agera",'manual'],
-  ["toyota","negro",'85','supra','auto'],
-  ['nissan','azul','847','nismo','manual']
+//var agencia =[
+  //[ "keonnisegg","rojo","1200","agera",'manual'],
+  //["toyota","negro",'85','supra','auto'],
+  //['nissan','azul','847','nismo','manual']
+//]
+//function Car(precio,transmision,modelo) {    //funcion constructora
+  //this.precio=precio
+  //this.transmision=transmision
+  //this.modelo=modelo
+
+//  this.getPrecio = function() {              //funcion por cada elemento del array
+    //return this.precio;
+  //}
+  //this.getTransmision = function () {
+    //return this.transmision;
+  //}
+//  this.getModelo = function () {
+  //  return this.modelo;
+  //}
+  //this.getDatos = function () {
+    //return this.precio + " " + this.transmision + " " + this.modelo;
+  //}
+
+//}
+//agencia.forEach(function (element) {         //instancia para obtener los elementos de array
+//  var car =  new Car (element[2], element [4], element [3])
+  //console.log(car.getDatos());
+//})
+
+
+
+
+
+var familia = [
+  ["nora","luna","mama","52"],
+  ["sara","contreras","abuelita","72"],
+  ["abertano","cervantes","tio","35"],
+  ["benjamin","luna","tio","53"],
+  ["daniel","frias","primo","27","herrero"],
+  ["jesus","salas","primo","23","tatuador"],
+  ["ivone","23"],
+  ["lizbeth","22"],
+
 ]
-function Car(precio,transmision,modelo) {
-  this.precio=precio
-  this.transmision=transmision
-  this.modelo=modelo
-
-  this.getPrecio = function() {
-    return this.precio;
-  }
-  this.getTransmision = function () {
-    return this.transmision;
-  }
-  this.getModelo = function () {
-    return this.modelo;
-  }
-  this.getDatos = function () {
-    return this.precio + " " + this.transmision + " " + this.modelo;
-  }
-
-}
-agencia.forEach(function (element) {
-  var car =  new Car (element[2], element [4], element [3])
-  console.log(car.getDatos());
-})
